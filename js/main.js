def6937a3f1bf879c44ada07ee59722ed1a943f0
golovanovv <46806234+golovanovv@users.noreply.github.com>
2021-02-03 18:01:55 +0400

    second commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const flowersSlider = new Swiper('.flowers-slider', {
+  // Optional parameters
+  slidesPerView: 6,
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
